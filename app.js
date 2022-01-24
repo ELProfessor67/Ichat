9ee18dd95a82f9c324fee5775b26f25e7be1d0bd
ELProfessor67 <jeeshanr599@gmail.com>
2022-01-24 15:59:52 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { Server } = require('http');
+const app = express();
+const path = require('path');
+const { Socket } = require('socket.io');
+
+// we will run Socket.io Server
+require('./chat');
+
+const port = process.env.PORT || 3000;
+
+// set public path
+const pubPath = path.join(__dirname,'public');
+app.use(express.static(pubPath));
+
+// set view engine
+app.set('view engine','hbs');
+
+// routing urls
+app.get('/',(req,res)=>{
+    res.render('index');
+});
+
+// server listen
+app.listen(port,()=>{
+    console.log(`http://127.0.0.1:${port}`);
+});
---
+++ public/js/main.js
@@ -0,0 +1,42 @@
+const socket = io('http://localhost:8000');
+
+const form = document.getElementById('send-container');
+const messageInp = document.getElementById('messageInp');
+const container = document.getElementById('container');
+
+const ding = new Audio('../ding.mp3');
+
+const append = (message,position) => {
+    const creteMessage = document.createElement('div');
+    creteMessage.innerText = message;
+    creteMessage.classList.add('message');
+    creteMessage.classList.add(position);
+    container.append(creteMessage);
+    if(position == 'left'){
+        ding.play();
+    }
+}
+
+const name = prompt('Enter your name to join');
+socket.emit('new-user-joined', name);
+
+
+socket.on('user-joined',(data)=>{
+    append(`${data} join the chat`,'left');
+});
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const message = messageInp.value;
+    append(`You: ${message}`,'right');
+    socket.emit('send',message);
+    messageInp.value = '';
+});
+
+socket.on('receive',(data)=>{
+    append(`${data.name}: ${data.message}`,'left')
+});
+
+socket.on('left',(data)=>{
+    append(`${data} left the chat`,'left');
+});
